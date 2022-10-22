b0e4fe902d0b1c08a7546bde8d157176523b3b8a
radek95 <34947422+radek95@users.noreply.github.com>
2022-10-22 14:16:54 +0200

    Add files via upload

---
+++ calc.php
@@ -0,0 +1,63 @@
+<?php
+// KONTROLER strony kalkulatora
+require_once dirname(__FILE__).'/../config.php';
+
+// W kontrolerze niczego nie wysyła się do klienta.
+// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
+// Parametry do widoku przekazujemy przez zmienne.
+
+// 1. pobranie parametrów
+
+$kwota = $_REQUEST ['kwota'];
+$liczba_lat = $_REQUEST ['liczba lat'];
+$wysokosc_oprocentowania = $_REQUEST ['wysokosc oprocentowania'];
+
+// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
+
+// sprawdzenie, czy parametry zostały przekazane
+if ( ! (isset($kwota) && isset($liczba lat) && isset($wysokosc oprocentowania))) {
+	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+}
+
+// sprawdzenie, czy potrzebne wartości zostały przekazane
+if ( $kwota == "") {
+	$messages [] = 'Nie podano kwoty';
+}
+if ( $liczba_lat == "") {
+	$messages [] = 'Nie podano liczby lat';
+}
+if ( $wysokosc_oprocentowania == "") {
+	$messages [] = 'Nie podano wysokosci oprocentowania';
+}
+
+//nie ma sensu walidować dalej gdy brak parametrów
+if (empty( $messages )) {
+	
+	// sprawdzenie, czy $x i $y są liczbami całkowitymi
+	if (! is_numeric( $kwota )) {
+		$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
+	}
+	
+	if (! is_numeric( $liczba_lat )) {
+		$messages [] = 'Druga wartość nie jest liczbą całkowitą';
+	}	
+
+}
+
+// 3. wykonaj zadanie jeśli wszystko w porządku
+
+if (empty ( $messages )) { // gdy brak błędów
+	
+	//konwersja parametrów na int
+	$kwota = intval($kwota);
+	$liczba_lat = intval($liczba_lat);
+
+	//wykonanie operacji
+	$result = ($kwota*$wysokosc_oprocentowania)/(12*(1-(12/(12+$wysokosc_oprocentowania))^$liczba_lat))$x - $y;
+}
+
+// 4. Wywołanie widoku z przekazaniem zmiennych
+// - zainicjowane zmienne ($messages,$x,$y,$operation,$result)
+//   będą dostępne w dołączonym skrypcie
+include 'calc_view.php';
